4a4f1c9bc4bb0e2fda1dc9265a69f46e47ef1b6d
Julian Cortes <mmm808383@gmail.com>
2024-10-23 09:06:43 -0500

    Creacion currentSong y function

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,7 @@ const rewind = document.getElementById('rewind');
 const stop = document.getElementById('stop');
 
 
+
 play.addEventListener('click', () => audio.play());
 pause.addEventListener('click', () => audio.pause());
 rewind.addEventListener('click', () => audio.currentTime -= 10);
@@ -18,6 +19,26 @@ stop.addEventListener('click', () => {
     audio.currentTime = 0;
 });
 
+
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
+
+
+
+let currentSong = 0;
+
+function skipSong(){
+    const loadSong = playList[currentSong];
+
+    document.getElementById('imgArtist').src = loadSong.img;
+    document.getElementById('nameSong').textContent = loadSong.title;
+    document.getElementById('nameArtist').textContent = loadSong.artist;
+    document.getElementById('audioSong').src = loadSong.Song;
+    document.getElementById('audio').load();
+
+}
+
+skipSong();
+
+
 
