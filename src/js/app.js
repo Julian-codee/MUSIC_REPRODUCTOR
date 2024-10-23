c44a81d6b6df891265c233c96b954cf2acb146fb
Julian Cortes <mmm808383@gmail.com>
2024-10-23 07:04:31 -0500

    Cambio guardado

--- src/js/app.js
+++ src/js/app.js
@@ -14,3 +14,36 @@ stop.addEventListener('click', () => {
     audio.pause();
     audio.currentTime = 0;
 })
+
+const playList = [
+    {
+      title: 'Song 1',
+      artist: 'Artist 1',
+      img: 'ruta de la imagen de la canción',
+      song: 'ruta de la cancion'
+    },
+    {
+      title: 'Song 2',
+      artist: 'Artist 2',
+      img: 'ruta de la imagen de la canción',
+      song: 'ruta de la cancion'
+    },
+    {
+      title: 'Song 3',
+      artist: 'Artist 3',
+      img: 'ruta de la imagen de la canción',
+      song: 'ruta de la cancion'
+    },
+    {
+      title: 'Song 4',
+      artist: 'Artist 4',
+      img: 'ruta de la imagen de la canción',
+      song: 'ruta de la cancion'
+    },
+    {
+      title: 'Song 5',
+      artist: 'Artist 5',
+      img: 'ruta de la imagen de la canción',
+      song: 'ruta de la cancion'
+    },
+  ]
