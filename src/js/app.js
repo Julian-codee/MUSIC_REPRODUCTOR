57f9738aecbe6dc97741bf9a9a50524defe880a8
Julian Cortes <mmm808383@gmail.com>
2024-10-23 07:08:26 -0500

    Cambio guardado

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+import { playList } from "./playList.js";
+
 const audio = document.getElementById('audio');
 const play = document.getElementById('play');
 const pause = document.getElementById('pause');
@@ -15,35 +17,3 @@ stop.addEventListener('click', () => {
     audio.currentTime = 0;
 })
 
-const playList = [
-    {
-      title: 'Song 1',
-      artist: 'Artist 1',
-      img: 'ruta de la imagen de la canción',
-      song: 'ruta de la cancion'
-    },
-    {
-      title: 'Song 2',
-      artist: 'Artist 2',
-      img: 'ruta de la imagen de la canción',
-      song: 'ruta de la cancion'
-    },
-    {
-      title: 'Song 3',
-      artist: 'Artist 3',
-      img: 'ruta de la imagen de la canción',
-      song: 'ruta de la cancion'
-    },
-    {
-      title: 'Song 4',
-      artist: 'Artist 4',
-      img: 'ruta de la imagen de la canción',
-      song: 'ruta de la cancion'
-    },
-    {
-      title: 'Song 5',
-      artist: 'Artist 5',
-      img: 'ruta de la imagen de la canción',
-      song: 'ruta de la cancion'
-    },
-  ]
---
+++ src/js/playList.js
@@ -0,0 +1,32 @@
+export const playList = [
+    {
+      title: 'Maldita Traga',
+      artist: 'Artist 1',
+      img: 'ruta de la imagen de la canción',
+      song: 'ruta de la cancion'
+    },
+    {
+      title: 'Song 2',
+      artist: 'Artist 2',
+      img: 'ruta de la imagen de la canción',
+      song: 'ruta de la cancion'
+    },
+    {
+      title: 'Song 3',
+      artist: 'Artist 3',
+      img: 'ruta de la imagen de la canción',
+      song: 'ruta de la cancion'
+    },
+    {
+      title: 'Song 4',
+      artist: 'Artist 4',
+      img: 'ruta de la imagen de la canción',
+      song: 'ruta de la cancion'
+    },
+    {
+      title: 'Song 5',
+      artist: 'Artist 5',
+      img: 'ruta de la imagen de la canción',
+      song: 'ruta de la cancion'
+    },
+  ]
